57677ad1dd5e061d797123a7777b67e7db9187ae
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-05-23 00:50:03 -0300

    Reflash

---
+++ useVariable.py
@@ -0,0 +1,5 @@
+var = 1
+accountBalance = 1000.0
+clientName = 'John Doe'
+print(var, accountBalance, clientName)
+print(var)
---
+++ variableNewValue.py
@@ -0,0 +1,9 @@
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+
+var = 100
+var = 200 + 300
+print(var)
