e4917f39aee08ffac23bbaac635e8f7274b3b993
Edivan Sousa <sousaep1974@gmail.com>
2022-07-28 23:22:36 -0300

    Update

--- cisco/4-Operators/operators.py
+++ cisco/4-Operators/operators.py
@@ -1,47 +1,54 @@
-print ("OPERATORS")
+print("OPERATORS")
 
-print ("Exponentiation ** (duble asterisk)")
+print("Exponentiation ** (duble asterisk)")
 print(2**3)
 print(2**3.)
 print(2.**3)
 print(2.**3.)
+print("--------------------------------------------------")
 
-print ("Multiplication * (asterisk)")
+print("Multiplication * (asterisk)")
 print(2 * 3)
 print(2 * 3.)
 print(2. * 3)
 print(2. * 3.)
+print("--------------------------------------------------")
 
-print ("Division / (slash)")
+print("Division / (slash)")
 print(6 / 3)
 print(6 / 3.)
 print(6. / 3)
 print(6. / 3.)
 print(1. / 2.)
 print(2 / 1)
+print("--------------------------------------------------")
 
-print ("Integer division (floor division) // (duble slash)")
+print("Integer division (floor division) // (duble slash)")
 print(6 // 3)
 print(6 // 3.)
 print(6. // 3)
 print(6. // 3.)
-print(6 // 5) 
-print (6 / 4)
+print(6 // 5)
+print(6 / 4)
+print("--------------------------------------------------")
 
-print ("Remainder (modulo) % (percent)") 
-#EXPLANATION: The rule still works:
-#3.0 – not 3 but 3.0. 
-#12 // 4.5 gives 2.0,
-#2.0 * 4.5 gives 9.0,
-#12 - 9.0 gives 3.0.
+print("Remainder (modulo) % (percent)")
+# EXPLANATION: The rule still works:
+# 3.0 – not 3 but 3.0.
+# 12 // 4.5 gives 2.0,
+# 2.0 * 4.5 gives 9.0,
+# 12 - 9.0 gives 3.0.
 print(14 % 4)
 print(12 % 4.5)
+print("--------------------------------------------------")
 
-print ("Addition + (plus)")
+print("Addition + (plus)")
 print(-4 + 4)
 print(-4. + 8)
+print("--------------------------------------------------")
 
-print ("The subtraction operator, unary and binary operators - (minus)")
+print("The subtraction operator, unary and binary operators - (minus)")
 print(-4 - 4)
 print(4. - 8)
 print(-1.1)
+print("--------------------------------------------------")
